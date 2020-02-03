bebcc121a7bdc5405b77db304b548b2d074cbfdb
Eric Choi <emchoi91@gmail.com>
2020-02-02 17:58:02 -0800

    Add non 2XX response handling for remote api shim

--- client/src/api/remote.js
+++ client/src/api/remote.js
@@ -1,31 +1,43 @@
+/**
+ * Object that acts as an API shim for the remote storage of show data.
+ */
+
 export default (url, key) => {
   const headers = {
     Authorization: `Bearer ${key}`,
     'Content-Type': 'application/json',
   };
 
+  // TODO: Research/follow best practices for FetchAPI error handling
+  const responseHandler = (response) => {
+    if (!response.ok) throw new Error(response.status);
+
+    return response;
+  };
+
   const getShows = () => fetch(url, {
     method: 'GET',
     headers,
   })
+    .then(responseHandler)
     .then(response => response.json());
 
   const addShow = info => fetch(url, {
     method: 'POST',
     headers,
     body: JSON.stringify(info),
-  });
+  }).then(responseHandler);
 
   const deleteShow = id => fetch(`${url}/${id}`, {
     method: 'DELETE',
     headers,
-  });
+  }).then(responseHandler);
 
   const editShow = (id, info) => fetch(`${url}/${id}`, {
     method: 'PUT',
     headers,
     body: JSON.stringify(info),
-  });
+  }).then(responseHandler);
 
   return {
     getShows,
