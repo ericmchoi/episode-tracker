26486485bb37eff2e4c353c86de89cbc2d365a3e
Eric Choi <emchoi91@gmail.com>
2020-02-04 16:50:44 -0800

    Add application packaging

---
+++ api/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ep-tracker-api',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_files=True,
+    zip_safe=False,
+    install_requires=['flask', 'flask-cors'],
+)
